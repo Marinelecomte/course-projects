26e8106e9b748990205e04ae2eee2b60d692597f
Marinelecomte <mclecomt@uio.no>
2025-09-03 05:51:00 -0400

    Update mesh_function.py

--- projects/mesh_function.py
+++ projects/mesh_function.py
@@ -5,16 +5,19 @@ from collections.abc import Callable
 def mesh_function(f: Callable[[float], float], t: np.ndarray) -> np.ndarray:
     return np.array([f(x) for x in t])
 
+
 def func(t: float) -> float:
     if t == 4:
         return np.exp(-12)
     return np.exp(-t)
+
 
 def test_mesh_function():
     t = np.array([1, 2, 3, 4])
     f = np.array([np.exp(-1), np.exp(-2), np.exp(-3), np.exp(-12)])
     fun = mesh_function(func, t)
     assert np.allclose(fun, f)
+
 
 if __name__ == "__main__":
     test_mesh_function()
