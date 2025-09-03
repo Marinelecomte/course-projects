42d95e78ccd8da94d7aeee244f25eb1cd675cdc3
Marinelecomte <mclecomt@uio.no>
2025-09-03 05:31:14 -0400

    Update differentiate.py

--- projects/differentiate.py
+++ projects/differentiate.py
@@ -2,10 +2,20 @@ import numpy as np
 
 
 def differentiate(u: np.ndarray, dt: float) -> np.ndarray:
-    raise NotImplementedError
+    du = np.zeros_like(u)
+    du[0] = (u[1] - u[0]) / dt
+    for i in range(1, len(u)-1):
+        du[i] = (u[i+1] - u[i-1]) / (2*dt)
+    du [-1] = (u[-1] - u[-2]) / dt
+    return du
 
 def differentiate_vector(u: np.ndarray, dt: float) -> np.ndarray:
-    raise NotImplementedError
+    du = np.zeros_like(u)
+    du[0] = (u[1] - u[0]) / dt
+    du[1:-1] = (u[2:] - u[:-2]) / (2*dt)
+    du[-1] = (u[-1] - u[-2]) / dt
+    return du
+
 
 def test_differentiate():
     t = np.linspace(0, 1, 10)
@@ -17,4 +27,3 @@ def test_differentiate():
 
 if __name__ == '__main__':
     test_differentiate()
-    
